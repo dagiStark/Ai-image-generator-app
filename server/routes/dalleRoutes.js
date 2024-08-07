1b431796ec54d144a0bb0d504bf320a326c4ea6f
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-08-07 19:42:43 +0300

    modified routes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,4 +1,19 @@
 import express from "express";
-import {Configuration, OpenAIApi} from 'openai'
+import   Configuration  from "openai";
+import   OpenAIApi  from "openai";
+import  dotenv from 'dotenv'
+dotenv.config()
 
 const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+  res.send("Hello from dall-e");
+});
+
+export default router;
