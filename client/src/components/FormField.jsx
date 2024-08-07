127cc31199805f699694d6435a2694aa5bf4fdf0
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-08-07 10:09:12 +0300

    finished client side form field

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,9 +1,64 @@
+import PropTypes from "prop-types";
 
-export const FormField = () => {
+export const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
   return (
-    <div>
-      form field
-    </div>
-  )
-}
+    <div className="mb-6">
+      <div className="flex items-center gap-2 mb-2">
+        <label htmlFor="" className="block text-sm font-medium text-gray-900">
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded=[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
 
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+        focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
+    </div>
+  );
+};
+
+FormField.propTypes = {
+  labelName: PropTypes.string.isRequired, // labelName should be a string and is required
+  type: PropTypes.string.isRequired, // type should be a string and is required
+  name: PropTypes.string.isRequired, // name should be a string and is required
+  placeholder: PropTypes.string.isRequired, // placeholder should be a string and is required
+  value: PropTypes.oneOfType([
+    // value can be a string or a number and is required
+    PropTypes.string,
+    PropTypes.number,
+  ]).isRequired,
+  handleChange: PropTypes.func.isRequired, // handleChange should be a function and is required
+  isSurpriseMe: PropTypes.bool, // isSurpriseMe should be a boolean and is optional
+  handleSurpriseMe: PropTypes.func, // handleSurpriseMe should be a function and is optional
+};
+
+// Default props
+FormField.defaultProps = {
+  isSurpriseMe: false, // Default value for isSurpriseMe is false
+  handleSurpriseMe: () => {}, // Default value for handleSurpriseMe is an empty function
+};
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,111 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 export const CreatePost = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {};
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+
+  const generateImage = () => {};
+
+  return (
+    <section className="separate max-w-7xl mx-auto ">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px] "> Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          Create imaginative and visually stunning images through DALL-E AI and
+          share them with your community
+        </p>
+      </div>
+
+      <form action="" className="blue-separate mt-16">
+        <div>
+          <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Jhon Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="prompt"
+            type="text"
+            name="prompt"
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+
+          <div
+            className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+          focus:ring-[#4649ff] focus:border-[#4649ff] w-64 p-3 h-64 flex justify-center items-center"
+          >
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.photo}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg ">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto  px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have created the image you want you can share it with
+            others in the community
+          </p>
+          <button
+            type="submit"
+            className="text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto  px-5 py-2.5 text-center"
+          >
+            Share with the community
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
